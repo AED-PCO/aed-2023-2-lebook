602f5ebf2c6d6db65228a4eb3aed1a87cb3f3a62
Ana Clara Marçal <limaclaraana15@gmail.com>
2023-12-10 16:18:48 -0300

    Conexão da tela de cadastro e o XML com a tela de login, para realizar as validações de login

--- LeBook/INICIO.cs
+++ LeBook/INICIO.cs
@@ -19,11 +19,9 @@ namespace LeBook
         }
 
         private void Form1_Load(object sender, EventArgs e)
-        {
-            // Carregar a imagem do projeto
-            Image Domquixote = Properties.Resources.Domquixote; // Certifique-se de substituir "MinhaImagem" pelo nome real da sua imagem
+{
+            Image Domquixote = Properties.Resources.Domquixote;
 
-            // Atribuir a imagem ao controle PictureBox
             pictureBox1.Image = Domquixote;
         }
 
@@ -49,18 +47,16 @@ namespace LeBook
 
         private void sAIRToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            // Exibir uma caixa de diálogo de confirmação
+
             DialogResult result = MessageBox.Show("Deseja realmente sair?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
 
-            // Se o usuário clicar em "Sim", fechar o programa
             if (result == DialogResult.Yes)
             {
                 Application.Exit();
             }
-            // Se o usuário clicar em "Não", não fazer nada
             else if (result == DialogResult.No)
             {
-                // Não fazer nada ou adicionar lógica adicional, se necessário
+
             }
         }
 
@@ -71,46 +67,37 @@ namespace LeBook
 
         private void lIVROSToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            // Criar uma instância do novo formulário
             Livros Livros = new Livros();
 
-            // Exibir o novo formulário
             Livros.Show();
         }
 
         private void cADASTROToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            // Criar uma instância do novo formulário
             QuartaTela QuartaTela = new QuartaTela();
-
-            // Exibir o novo formulário
             QuartaTela.Show();
         }
 
         private void lOGINToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            // Criar uma instância do novo formulário
             SegundaTela segundaTela = new SegundaTela();
 
-            // Exibir o novo formulário
             segundaTela.Show();
         }
 
         private void dEVOLUÇÃOToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            // Criar uma instância do novo formulário
+
             Devolução Devolução = new Devolução();
 
-            // Exibir o novo formulário
             Devolução.Show();
         }
 
         private void aJUDAToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            // URL do site externo
             string url = "https://answers.microsoft.com/pt-br/msoffice/forum/all/suporte-para-microsoft-forms/0eb71c22-efd8-4479-8e02-fffd6616f090";
 
-            // Abrir o navegador padrão com a URL
+
             Process.Start(url);
         }
     }
--- LeBook/SegundaTela.cs
+++ LeBook/SegundaTela.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace LeBook
 {
@@ -16,21 +18,66 @@ namespace LeBook
         {
             InitializeComponent();
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
-
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+        
+        
+        private List<Pessoa> CarregarDadosPessoas()
+        {
+            List<Pessoa> listaPessoas = new List<Pessoa>();
 
+            if (File.Exists(Environment.CurrentDirectory + "\\DadosPessoas.xml"))
+            {
+                XmlSerializer serial = new XmlSerializer(typeof(List<Pessoa>));
+
+                using (FileStream fs = new FileStream(Environment.CurrentDirectory + "\\DadosPessoas.xml", FileMode.Open, FileAccess.Read))
+                {
+                    listaPessoas = serial.Deserialize(fs) as List<Pessoa>;
+                }
+            }
+            return listaPessoas;
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private bool VerificarCredenciais(string email, string senha)
+        {
+            List<Pessoa> listaPessoas = CarregarDadosPessoas();
+            return listaPessoas.Any(p => p.email == email && p.senha == senha);
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string emailDigitado = textBox2.Text;
+            string senhaDigitada = maskedTextBox1.Text;
+
+            if (VerificarCredenciais(emailDigitado, senhaDigitada))
+            {
+                MessageBox.Show("Login bem-sucedido!");
+                this.Close();
+             
+                Livros Livros = new Livros();
+                Livros.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Credenciais inválidas. Tente novamente.");
+            }
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
         {
 
         }
+
     }
 }
 
