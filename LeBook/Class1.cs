e5a2027aa1a72284c55a6ad9b4d3d8aaf9806bab
Ana Clara Marçal <limaclaraana15@gmail.com>
2023-12-10 15:41:26 -0300

    Criação do arquivo XML e a página de Cadastro

---
+++ LeBook/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeBook
+{
+    public class Pessoa
+
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public string email { get; set; }
+
+        public string cpf { get; set; }
+
+        public string telefone { get; set;}
+
+        public string senha { get; set; }
+
+        public string cep { get; set; }
+    }
+}
